68ec84ebf65f818a9bbefacfe43005170114895b
Jazz <jazz.celaya@tursagroup.com>
2019-12-23 03:13:58 -0600

    adds sudocode for  endpoints

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,16 @@ from app import app
 
 @app.route('/')
 def start():
-    return "“I break away from all conventions that do not lead to my earthly success and happiness.”"
+    return "I break away from all conventions that do not lead to my earthly success and happiness"
+
+
+@app.route('/upload')
+def upload():
+    return "we will upload files here"
+    # POST https://api-app.xtracta.com/v1/documents
+
+@app.route('/download')
+def download():
+    return "this endpoint will receive a doc ID and download its data from xtracta"
+    # POST https://api-app.xtracta.com/v1/documents
+    
