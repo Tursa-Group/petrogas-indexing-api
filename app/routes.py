ce24625aeb2976cf2790188107bfd9ae121e5ca6
AlejandroRodrigueP <a.rodriguez.p.1995@gmail.com>
2019-12-26 00:30:14 -0600

    hosted on heroku, working on routes now

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,14 @@ import requests
 
 @app.route('/')
 def start():
-    return "<h1>Xtracta API <h2(created with FLASK)/></h1>"
+    return "<h1>Xtracta API </h1> <h4> Created with Flask</h4>"
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    r = requests.post('https://api-app.xtracta.com/v1/documents/upload')
-    return r.json()
+    r=requests.post('https://api-app.xtracta.com/v1/documents/upload')
+    return 'posts'
     # POST https://api-app.xtracta.com/v1/documents/upload
 
 
-@app.route('/download/', methods=['POST'])
-def download():
-    r = requests.post('https://api-app.xtracta.com/v1/documents')
-    return r.json()
     # POST https://api-app.xtracta.com/v1/documents
