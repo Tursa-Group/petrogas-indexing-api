dbaa1e3724e00769216536315eef969bf924996a
AlejandroRodrigueP <a.rodriguez.p.1995@gmail.com>
2019-12-29 01:12:22 -0600

    dowload_file endpoint is working, need to test throughly

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from app import app
 import requests
 import urllib.parse
 
+
+
 @app.route('/')
 def start():
     return '''
@@ -17,29 +19,44 @@ def start():
          '''
 
 
-@app.route('/upload')
+#relative_path = app/test.pdf
+
+#upload-route
+@app.route('/upload_file')
 def upload():
+    #files and upload_url variables
+    files = {'file': open('app/test.pdf','rb')}
+    upload_url ='https://api-app.xtracta.com/v1/documents/upload'
+    header= {"Content-Type": "application/x-www-form-urlencoded"}
+
+    #auth keys and file to be uploaded
     auth_upload={
         'api_key':'b65d6427252e69e4aa29728f6ebfbf43ccf2f266',
-        'workflow_id':963111,
-        'userfile':''
+        'workflow_id':'963111',
+        'userfile': 'files'
     }
-    upload_url ='https://api-app.xtracta.com/v1/documents/upload'
-    r=requests.post(upload_url, data = auth_upload)
-    return r.text
+
+    # POST request to upload pdf file
+    r=requests.post(url=upload_url, data=auth_upload, headers=header)
+   # return r.text
+    return str(r.content)
 
 
-#/dowload route 
-@app.route('/down-load')
-def download():
+
+#/dowload route
+@app.route('/download_file/<doc_id>')
+def download(doc_id):
+    #download url variable
+    download_url='https://api-app.xtracta.com/v1/documents/'
+
+    #auth keys and download=document_id
     auth_download={
         'api_key':'b65d6427252e69e4aa29728f6ebfbf43ccf2f266',
-        'workflow_id':963111,
-        'document_id':136445044
+        'workflow_id': '963111',
+        'document_id': doc_id
     }
-    download_url='https://api-app.xtracta.com/v1/documents/download'
-    r=requests.post(download_url, data = auth_download)
 
-    return str(r.status_code)
-
-# POST https://api-app.xtracta.com/v1/documents
+    #POST request
+    r=requests.post(url=download_url,data=auth_download)
+    #return content 
+    return str(r.content)
